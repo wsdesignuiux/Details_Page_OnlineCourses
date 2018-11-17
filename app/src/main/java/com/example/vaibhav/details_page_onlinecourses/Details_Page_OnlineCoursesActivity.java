55d7b111f278160acfc35c4e90a4cde32202b8b6
Rushabh Patel <wsdesignuiux@gmail.com>
2018-11-17 12:02:27 +0530

    Initial commit

---
+++ app/src/main/java/com/example/vaibhav/details_page_onlinecourses/Details_Page_OnlineCoursesActivity.java
@@ -0,0 +1,13 @@
+package com.example.vaibhav.details_page_onlinecourses;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Details_Page_OnlineCoursesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_details__page__online_courses);
+    }
+}
